298cf15824b4006f19734010d564fca12360db42
Evi <evicoach@gmail.com>
2019-09-16 16:43:24 +0100

    Added nav bar with search form and a mock list of products: basic user interface

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+function newProduct() { }
+function readAllProducts() { }
+function updateProducts() { }
+function deleteProducts() { }
+function fetchOne() { }
+
+$(document).ready(function () {
+
+    const $newProduct = $("#new-product");
+    const $allProducts = $("#all-products");
+    const $updateProduct = $("#update-products");
+    const $deleteProduct = $("#delete-product");
+    const $fetchOne = $("#fetch-one");
+
+    $newProduct.on('click', e => {
+        
+    })
+});
